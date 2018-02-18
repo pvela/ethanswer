197e4cde11b2f5e34fb5cd1417cb9e80b6b6acf3
Vivek <shahvicky1992@gmail.com>
2018-02-18 00:26:13 -0600

    write-access

---
+++ controllers/postAnswer.js
@@ -0,0 +1,9 @@
+const SE_BASE_URL='https://api.stackexchange.com/2.2/'
+const API_KEY='ekGQbH8mopUzetlfSTPUrQ((';
+
+function postAnswer(req,res,next) {
+    const questionId = req.id;
+    const accessToken = req.accessToken;
+    const body = req.body;
+    
+}
